c8af8e891c4b9df4f70b110a5ef7092f3e8db5cf
unknown <n.niedziela201@gmail.com>
2020-11-16 06:41:58 +0100

    routing

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <ul>
+        <li><router-link to='/'>Stock Trader</router-link></li>
+        <li><router-link to='/Portfolio'>Portfolio</router-link></li>
+        <li><router-link to='/Stocks'>Stocks</router-link></li>
+    </ul>
+</template>
+<script>
+export default {
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-import router from './router'
+import { routes } from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 new Vue({
   router,
