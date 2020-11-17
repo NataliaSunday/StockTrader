9befd703f3342d3e31b6fc93638f549c2217a28f
unknown <n.niedziela201@gmail.com>
2020-11-17 19:25:10 +0100

    add vue bootsrap

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { routes } from './router/router.js'
 import store from './store'
 
+Vue.use(BootstrapVue)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
